014f6d8474b46a8aa70f2b953063b45868b11c6c
Harrish92 <harrishan.raveendran@epfl.ch>
2026-01-07 14:56:43 +0100

    feat: write function that underlines self referencing prs.

--- source/features/highlight-pr-self-closing-link.tsx
+++ source/features/highlight-pr-self-closing-link.tsx
@@ -1,18 +1,52 @@
 import * as pageDetect from 'github-url-detection';
 import features from '../feature-manager.js';
 import observe from '../helpers/selector-observer.js';
+import { getConversationNumber } from '../github-helpers/index.js';
+import getTextNodes from '../helpers/get-text-nodes.js';
 
-function funcDoesThings(): void {
-	console.log(1);
+function funcDoesThings(prConversation: HTMLElement): void {
+	const prNumber = getConversationNumber();
+
+	const textNodesOnThisLine = getTextNodes(prConversation);
+	for (const [, textNode] of textNodesOnThisLine.entries()){
+		let text = textNode.textContent;
+		let prSelfReference = '#' + prNumber!.toString();
+
+		// Find subsequent occurrences
+		let searchFromIndex: number = 0;
+		const indices: number[] = [];
+
+		while ((searchFromIndex = text.indexOf(prSelfReference, searchFromIndex)) !== -1) {
+			indices.push(searchFromIndex);
+			searchFromIndex += prSelfReference.length;
+		}
+
+		// Split to create a separate node containing the self-referenced link
+		for (var indice of indices) {
+			textNode.splitText(indice);
+			const endingCharacterIndex = indice + prSelfReference.length - 1;
+			if (endingCharacterIndex < text.length - 1){
+				textNode.splitText(endingCharacterIndex);
+			}
+			textNode.parentElement!.style.textDecoration = 'underline wavy red';
+		}
+	}
 }
 
 function init(signal: AbortSignal): void {
-	observe('[input="fc-pull_request_body"]', funcDoesThings, {signal});
+	observe([
+		'.comment-body',
+		'.markdown-body',
+		'.js-comment-body',
+		'.soft-wrap',
+		'.user-select-contain',
+		'.d-block',
+	], funcDoesThings, {signal});
 }
 
 void features.add(import.meta.url, {
 	include: [
-		pageDetect.isCompare, // Find the one you need on https://refined-github.github.io/github-url-detection/
+		pageDetect.isPR, // Find the one you need on https://refined-github.github.io/github-url-detection/
 	],
 	init,
 });
